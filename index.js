1868f06e4bbcc766548f841f5d09ef9e0cd2bed0
Malik Kotb <malikkotb@icloud.com>
2024-02-02 13:42:06 +0100

    schema defined

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone"; // so we can listen for requests
+import { typeDefs } from "./schema";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+// server setup
+// the ApolloServer takes in 2 properties: typeDefs (your gql schema) and resolvers
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+console.log("Server ready at port", 4000);
---
+++ schema.js
@@ -0,0 +1,40 @@
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+
+// Built-in scalar types: int, float, string, boolean, ID (gql uses ID as a key for data objects)
+
+export const typeDefs = `#graphql
+
+    type Game {
+        id: ID! # '!' makes this field required & can't be null
+        title: String!
+        platform: [String!]! # array of strings
+    }
+
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+    }
+
+    # The "Query" type is special: it lists all of the available queries that
+    # clients can execute, along with the return type for each.
+    # NOT OPTIONAL, it's job is to define the entry points to the graph and specify
+    # the return types of those entry points
+    # so here you definde where a user can jump into the graph initially
+    # or where queries can start from 
+
+    type Query {
+        reviews: [Review]
+        games: [Game]
+        authors: [Author] 
+    } 
+
+`;
